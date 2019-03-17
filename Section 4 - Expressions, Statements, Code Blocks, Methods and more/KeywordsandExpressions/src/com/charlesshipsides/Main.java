f2ce28fd6f55c037c5276ed881a9c7cb10489198
ChuckyEpic <chuckyepic31804@gmail.com>
2019-03-17 22:28:17 +0000

    Section 4 Started

---
+++ Section 4 - Expressions, Statements, Code Blocks, Methods and more/KeywordsandExpressions/src/com/charlesshipsides/Main.java
@@ -0,0 +1,22 @@
+package com.charlesshipsides;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Can't use keywords as variables etc
+        int int2 = 5;
+
+        double kilometeres = (100 * 1.609344);
+        int highScore = 50;
+        if(highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
+        // Examples of expressions:
+        // score = 100
+        // score > 99
+        // "You got the high score"
+        // score = 0
+    }
+}
