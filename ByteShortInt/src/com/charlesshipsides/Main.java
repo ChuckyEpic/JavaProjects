681a3449eaf300c59ed90d0255634d9ae51ab2bf
ChuckyEpic <chuckyepic31804@gmail.com>
2019-03-17 13:22:32 +0000

    ByteShortInt

---
+++ ByteShortInt/src/com/charlesshipsides/Main.java
@@ -0,0 +1,9 @@
+package com.charlesshipsides;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myValue = 10000;
+
+    }
+}
--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,5 +4,13 @@ public class Hello {
 
         System.out.println("Hello, Charles!");
 
+        int myFirstNumber = (5 + 10) * (5 + 5);
+        int mySecondNumber = 12 / 6;
+        int myThirdNumber = 6;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotal);
+
     }
 }
